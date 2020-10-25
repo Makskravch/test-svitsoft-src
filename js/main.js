001c1aca5fb6f9b2a62960fc880d25f4ef541f94
Maksym Kravchuk <kravchukmaxym@gmail.com>
2020-10-25 11:35:55 +0200

    Test completed

--- js/main.js
+++ js/main.js
@@ -1,10 +1,23 @@
 $(document).ready(function(){
+
+  let body = $('body')
+  let menu = $('.menu')
+
+  $('.js-btn-menu-toggle').on('click', function () {
+    body.toggleClass('overflow')
+    $(this).toggleClass('is-active')
+    menu.toggleClass('is-open')
+  })
+
+
   $('.banner-slider').slick({
     arrows: false
   });
   $('.banner-slider-small').slick({
     arrows: false,
     dots: true,
+    draggable: false,
+    swipe: false,
     customPaging : function(slider, i) {
       if (i < 9) {
         return `<button>0${i+1}</button>`;
